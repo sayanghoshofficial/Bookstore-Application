d79e3dc5ce8098963ad7968693a98457621c31b3
Sayan Ghosh <sayanghosh379@gmail.com>
2023-06-19 20:48:35 +0530

    create context and search bar

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppProvider } from "./context";
 import "./index.css";
 import { About, Home } from "./pages";
-import {BookDetails, BookList} from "./component";
+import { BookDetails, BookList } from "./component";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />}>
-        <Route path="about" element={<About />} />
-        <Route path="book" element={<BookList/>}/>
-        <Route path="/book/:id" element={<BookDetails/>}/>
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="about" element={<About />} />
+          <Route path="book" element={<BookList />} />
+          <Route path="/book/:id" element={<BookDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
 );
