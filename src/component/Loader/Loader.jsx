75e2f0532065d85c72fe685a4181928022042629
Sayan Ghosh <sayanghosh379@gmail.com>
2023-07-29 18:05:32 +0530

    change the loader

--- src/component/Loader/Loader.jsx
+++ src/component/Loader/Loader.jsx
@@ -4,15 +4,7 @@ import './Loader.css'; // Add CSS file for styling
 const Loader = () => {
   return (
     <div className="loader-container">
-      <div className="loader">
-        <div className="loader-inner"></div>
-        <div className="loader-inner"></div>
-        <div className="loader-inner"></div>
-        <div className="loader-inner"></div>
-        <div className="loader-inner"></div>
-        <div className="loader-inner"></div>
-        <div className="loader-inner"></div>
-      </div>
+      <div className="custom-loader"></div>
     </div>
   );
 };
